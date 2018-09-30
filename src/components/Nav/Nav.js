36cb515f08eb11f77decf367f0684668a9c1f7de
rzhancock <rzhancock@gmail.com>
2018-09-30 02:28:39 -0700

    Nav styling complete. Need to add smooth scrolling

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,14 +5,20 @@ import logo from './logo.png';
 function Nav() {
 		return ( 
 			<header className="header">
+					<div className="name-wrapper">
 					<h1><a className="name" href="#">ROBERT HANCOCK</a></h1>
+					</div>
 					<nav>
 			     	  <ul className="main-nav">
+			     	  	<li><a>Skills</a></li>
 				        <li><a>Projects</a></li>
 						<li><a>History</a></li>
 						<li><a>Contact</a></li>
-						<li><a id="blog-link">Blog
-						<div id="blog-subscript">Coming Soon!</div></a></li>
+						<li><a>
+							<span className="blog-link">Blog</span>
+							<span className="blog-subscript">Coming Soon!</span>
+							</a>
+						</li>
 			      	  </ul>
 					</nav>
 			</header>
