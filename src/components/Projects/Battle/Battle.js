bcd4898157753d861e86e3d13775c81368dfb352
rzhancock <rzhancock@gmail.com>
2018-10-18 23:53:55 -0700

    Finished mern project and updated styling for both mern and battle

--- src/components/Projects/Battle/Battle.js
+++ src/components/Projects/Battle/Battle.js
@@ -12,7 +12,7 @@ function Battle() {
               <a href="https://twitter.com/tylermcginnis" className="tmcginnis" target="_blank"> Tyler McGiniss</a>
               . It provides an excellent introduction to React.js, AJAX with Axios, Webpack, Babel, React Router v4, and Flexbox.
             </p>
-            <a href="https://roberthancock.net/github-battle" target="_blank" className="demo">View Live Demo</a>
+            <a href="https://roberthancock.net/github-battle" target="_blank" className="battle-demo">View Live Demo</a>
             <div className="battle-images">
               <img src={battle_png} alt="GitHub Battle" className="battle-png" />
               <img src={popular_png} alt="Popular GitHub Repos" className="popular-png" />
--- src/components/Projects/Mern/Mern.js
+++ src/components/Projects/Mern/Mern.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Mern.css';
+import main_png from './shoppinglistmain.PNG';
+import add_png from './shoppinglistadd.PNG';
 
 
 function Mern() {
@@ -9,8 +11,10 @@ function Mern() {
             <p className="mern-description">
               A simple peristent shopping list created as part of the<a href="https://www.youtube.com/playlist?list=PLillGF-RfqbbiTGgA77tGO426V3hRF9iE"> LEARN the MERN Stack </a>series by <a href="http://www.traversymedia.com/#about"> Brad Traversy</a>. This project utilizes the Express API with a MongoDB database hosted by mLab for the backend with React, Redux, Bootstrap 4, and the React Transition Group module on the front end. 
             </p>
-            <a href="http://mern.roberthancock.net" target="_blank" className="demo">View Live Demo</a>
+            <a href="http://mern.roberthancock.net" target="_blank" className="mern-demo">View Live Demo</a>
             <div className="mern-images">
+              <img src={main_png} alt="Shopping List Main Page" className="main-png" />
+              <img src={add_png} alt="Shopping List Add Modal" className="addr-png" />
             </div>
           </div>
 
